49e064e0d809ed171bc779157ad1cac810548e6d
Nigel Faria <nigel.faria@gmail.com>
2022-12-12 20:13:44 +0000

    Ex: Rest Params & Spread Syntax

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,8 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(a, ...theRest) {  
+  return a + theRest.reduce((sum, number)=> sum + number, 0 );
 }
 
 add(1, 2, 3, 4, 5);
 
-module.exports = add;
